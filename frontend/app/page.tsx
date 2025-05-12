acc1e4eecff6d6adbc6540fdcd4b48033184e416
arafatDU <bsse1435@iit.du.ac.bd>
2025-05-12 10:28:33 +0600

    Frontend project initialized

---
+++ frontend/app/page.tsx
@@ -0,0 +1,14 @@
+'use client';
+
+
+
+export default function Home() {
+
+  return (
+    <main className="min-h-screen bg-background">
+      <div className="container mx-auto px-4 py-8">
+        Hello world!
+      </div>
+    </main>
+  );
+}
